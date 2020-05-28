4c4bbc9d3f6be14fb0df18168e9be2a83c7a1a2a
Phill Barron <Phill.Barron@bbc.co.uk>
2020-05-28 16:11:17 +0100

    todoing

--- src/babel-passport-mapper.js
+++ src/babel-passport-mapper.js
@@ -2,7 +2,8 @@ const mapToPassport = babelMessage => {
   const { programme: { languages, taggings }} = babelMessage;
   const language = languages.length > 0 ? languages[0] : undefined;
   const locator = `urn:bbc:pips:pid:${babelMessage.programme.pid}`;
-  const home = taggings.length > 0 ? taggings[0] : undefined;
+  const home = taggings.length > 0 ? taggings[0] : undefined; // add homes mapping here
+  // todo get the tags
   return {
     locator,
     language,
