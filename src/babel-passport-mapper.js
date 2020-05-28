0b00569689d3647b5c215fc20dfbcbfc94cbc257
Phill Barron <Phill.Barron@bbc.co.uk>
2020-05-28 18:57:29 +0100

    home mapping

--- src/babel-passport-mapper.js
+++ src/babel-passport-mapper.js
@@ -1,8 +1,12 @@
+
+const getHome = homeKey => `http://www.bbc.co.uk/ontologies/passport/home/${homeKey}`;
+
 const mapToPassport = babelMessage => {
   const { programme: { languages, taggings }} = babelMessage;
+  const home = taggings.length > 0 ? taggings[0] : undefined;
+  if (!home) return;
   const language = languages.length > 0 ? languages[0] : undefined;
   const locator = `urn:bbc:pips:pid:${babelMessage.programme.pid}`;
-  const home = taggings.length > 0 ? taggings[0] : undefined; // add homes mapping here
   // todo get the tags
   return {
     locator,
