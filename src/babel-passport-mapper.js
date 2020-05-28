d629166050a1ff08e7efbcab66ac96400656c39e
Phill Barron <Phill.Barron@bbc.co.uk>
2020-05-28 16:10:37 +0100

    begin to map home

--- src/babel-passport-mapper.js
+++ src/babel-passport-mapper.js
@@ -1,10 +1,12 @@
 const mapToPassport = babelMessage => {
-  const { programme: { languages }} = babelMessage;
+  const { programme: { languages, taggings }} = babelMessage;
   const language = languages.length > 0 ? languages[0] : undefined;
   const locator = `urn:bbc:pips:pid:${babelMessage.programme.pid}`;
+  const home = taggings.length > 0 ? taggings[0] : undefined;
   return {
     locator,
     language,
+    home
   };
 };
 
