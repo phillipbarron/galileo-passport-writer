44c3e2cde30431fe3bf3a9263891b82d6bcd23f6
Phill Barron <phillipbarron242@gmail.com>
2020-05-28 20:33:39 +0100

    a=fixed all the fail

--- src/babel-passport-mapper.js
+++ src/babel-passport-mapper.js
@@ -1,17 +1,27 @@
 
 const getHome = homeKey => `http://www.bbc.co.uk/ontologies/passport/home/${homeKey}`;
 
+const getNewsCategory = categoriesArray => {
+  console.log({ categoriesArray });
+  return getHome(categoriesArray.filter(category => category.namespace === "news_categories")[0].tags[0]);
+};
+
 const mapToPassport = babelMessage => {
-  const { programme: { languages, taggings }} = babelMessage;
-  const home = taggings.length > 0 ? taggings[0] : undefined;
+  const { programme: { languages, tags, taggings: categories }} = babelMessage;
+  console.info({ categories });
+  const home = categories.length > 0 ? getNewsCategory(categories) : undefined;
   if (!home) return;
   const language = languages.length > 0 ? languages[0] : undefined;
   const locator = `urn:bbc:pips:pid:${babelMessage.programme.pid}`;
-  // todo get the tags
+  const taggings = tags && tags.types.about ? tags.tags.types.about.map(tag => ({
+    predicate: "http://www.bbc.co.uk/ontologies/passport/predicate/About",
+    value: tag,
+  })) : undefined;
   return {
     locator,
     language,
-    home
+    home,
+    tags
   };
 };
 
--- src/services/passport-event-sevice.js
+++ src/services/passport-event-sevice.js
@@ -3,32 +3,37 @@
 const passportMapper = require("../babel-passport-mapper");
 const passportApiClient = require('../passport-api-client');
 const passporWriterClient = require('../passport-writer-client');
-const deltaService = require('@bbc/passport-event-generator');
+const generatePassportEvent = require('@bbc/passport-event-generator');
 
 const service = {
-  serviceIdentifier: 'galileo-passport-writer',
-  serviceVersion: '1.0.0'
-}
+  identifier: 'galileo-passport-writer',
+  version: '1.0.0'
+};
+
 const sendPassportEvent = async babelMessage => {
   // build passport
   console.log('building passport with', babelMessage);
-  const { audit: { publisher: { id: user } }} = babelMessage;
+  const { audit: { user: { id: user } }} = babelMessage;
+  console.log('AND THE USER IS ', user);
   const passportFromMessage = passportMapper.mapToPassport(babelMessage);
   console.info({ passportFromMessage });
   // get current state
-  const currentPassport = await passportApiClient.getPassport(passportFromMessage.locator);
-  console.info({ currentPassport });
+  // const currentPassport = passportFromMessage ? await passportApiClient.getPassport(passportFromMessage.locator): {};
   // build delta
-  const delta = deltaService.generatePassportEvent({
-    oldPassport: currentPassport,
-    newPassport: passportFromMessage,
-    user,
-    service
-  });
-
-  console.info({ delta });
-  // write to passport writer
-  // return passporWriterClient.sendPassportEvent(delta);
+  if (passportFromMessage) {
+    const delta = generatePassportEvent.default({
+      oldPassport: {},
+      newPassport: passportFromMessage,
+      user,
+      service
+    });
+  
+    console.info({ delta: JSON.stringify(delta) });
+    return;
+    // write to passport writer
+    // return passporWriterClient.sendPassportEvent(delta);
+  }
+  console.info("insufficient data to create passport");
 }
 
 module.exports = {
